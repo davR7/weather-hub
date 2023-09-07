4f326b02e12926346ca1e9929288c32656a77b3d
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-07 12:08:36 -0300

    created - Wheather Component

--- src/pages/Weather/index.tsx
+++ src/pages/Weather/index.tsx
@@ -1,5 +1,18 @@
+import * as S from './style';
+import ClimateDetails from '../../components/ClimateDetails';
+import ClimateSummary from '../../components/ClimateSummary';
+import SearchBar from '../../components/SearchBar';
+
 function Weather() {
-  return <></>;
+  return (
+    <S.Container>
+      <S.Wheather>
+        <SearchBar />
+        <ClimateSummary />
+        <ClimateDetails />
+      </S.Wheather>
+    </S.Container>
+  );
 }
 
 export default Weather;
