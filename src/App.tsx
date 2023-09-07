76e816bacce7fbdac9284b8f58cf33c1dfa68a91
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-07 12:14:28 -0300

    updated - App arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
+import Weather from './pages/Weather';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import Home from './pages/Weather';
 
-function App() {
+const App: React.FC = () => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <Weather />
     </QueryClientProvider>
   );
-}
+};
 
 export default App;
