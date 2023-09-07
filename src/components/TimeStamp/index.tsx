d472fc33fc9c3c0853b4b9278bcc4e219ecda387
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-07 12:11:43 -0300

    created - TimeStamp component

---
+++ src/components/TimeStamp/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const TimeStamp: React.FC = () => {
+  return <S.TimeStamp>8:00 Fri, 7 September</S.TimeStamp>;
+};
+
+export default TimeStamp;
---
+++ src/components/TimeStamp/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const TimeStamp = styled.div`
+  color: #fff;
+`;
