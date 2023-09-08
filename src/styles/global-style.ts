98f2475c616166439843b1df3106656ac6bcbab0
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-08 11:23:46 -0300

    updated - removed global image style

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -4,7 +4,6 @@ export const GlobalStyle = createGlobalStyle`
   * {margin: 0; padding: 0; border: none; outline: none; font-size: 100%; box-sizing: border-box;}
   ul {list-style: none}
   a {text-decoration: none;}
-  img {width: 100%}
   body {font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif; line-height: 1.5; 
     text-rendering: optimizeLegibility; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;}
 `;
