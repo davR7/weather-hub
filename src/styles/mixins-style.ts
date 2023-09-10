3ba2d526746c99f4cc3314c7e68033aca3142858
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-09 23:24:27 -0300

    updated - new mixins style

--- src/styles/mixins-style.ts
+++ src/styles/mixins-style.ts
@@ -1,5 +1,11 @@
 import { css } from 'styled-components';
 
+export const FlexRow_C_XY = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const FlexRow_C_Y = css`
   display: flex;
   align-items: center;
@@ -9,5 +15,11 @@ export const FlexColumn_C_XY = css`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+`;
+
+export const FlexColumn_C_X = css`
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `;
