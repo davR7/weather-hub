93834c5d5c7bbb57fc7c086f83ae62d4d429b724
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-09 23:40:35 -0300

    updated - added error and setError

--- src/contexts/weather/WeatherContext.ts
+++ src/contexts/weather/WeatherContext.ts
@@ -1,9 +1,17 @@
 import { createContext } from 'react';
 import { WeatherProps } from '../../types/WeatherProps';
+import { ErrorProps } from '../../types/ErrorProps';
 
-export type ContextProps = {
+type ContextProps = {
   weather: WeatherProps | null;
   setWeather: React.Dispatch<React.SetStateAction<WeatherProps | null>>;
+  error: ErrorProps | null;
+  setError: React.Dispatch<React.SetStateAction<ErrorProps | null>>;
 };
 
-export const WeatherContext = createContext<ContextProps | null>(null);
+export const WeatherContext = createContext<ContextProps>({
+  weather: null,
+  setWeather: () => {},
+  error: null,
+  setError: () => {},
+});
--- src/contexts/weather/WeatherProvider.tsx
+++ src/contexts/weather/WeatherProvider.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { WeatherContext } from './WeatherContext';
 import { WeatherProps } from '../../types/WeatherProps';
 import { useApi } from '../../hooks';
+import { ErrorProps } from '../../types/ErrorProps';
 
 type ProviderProps = {
   children: React.ReactNode;
@@ -9,12 +10,22 @@ type ProviderProps = {
 
 export const WeatherProvider: React.FC<ProviderProps> = ({ children }) => {
   const [weather, setWeather] = useState<WeatherProps | null>(null);
-  const { VITE_API_INIT_URL } = import.meta.env;
+  const [error, setError] = useState<ErrorProps | null>(null);
+  const { VITE_API_INIT_URL, VITE_API_KEY } = import.meta.env;
   const api = useApi();
 
-  if (!weather) {
-    api.findWeather<WeatherProps>(VITE_API_INIT_URL).then(data => setWeather(data));
+  if (!weather && !error) {
+    api
+      .findWeather<WeatherProps>(`${VITE_API_INIT_URL}&APPID=${VITE_API_KEY}&lang=pt_br`)
+      .then(res => {
+        setWeather(res.data);
+        setError(res.error);
+      })
+      .catch(err => {
+        setWeather(err.data);
+        setError(err);
+      });
   }
 
-  return <WeatherContext.Provider value={{ weather, setWeather }}>{children}</WeatherContext.Provider>;
+  return <WeatherContext.Provider value={{ weather, setWeather, error, setError }}>{children}</WeatherContext.Provider>;
 };
