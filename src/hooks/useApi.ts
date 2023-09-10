da0c099871f567d0f503c042f15687aeb7f18cc2
Davidson Ratis <davidsonmakarios@gmail.com>
2023-09-09 23:31:20 -0300

    updated - added property clientMessage

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { FetchProps } from '../types/FetchProps';
 const { VITE_API_BASE_URL } = import.meta.env;
 
 const http = axios.create({
@@ -6,8 +7,19 @@ const http = axios.create({
 });
 
 export const useApi = () => ({
-  async findWeather<T>(endpont: string): Promise<T> {
-    const response = await http.get<T>(endpont);
-    return response.data;
+  async findWeather<T>(endpont: string): Promise<FetchProps<T>> {
+    try {
+      const response = await http.get<T>(endpont);
+      return { data: response?.data, error: null };
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        return { data: null, error: { ...e.response?.data, clientMessage: 'cidade não encontrada' } };
+      } else {
+        return {
+          data: null,
+          error: { cod: '0', message: 'unexpected error', clientMessage: 'cidade não encontrada' },
+        };
+      }
+    }
   },
 });
